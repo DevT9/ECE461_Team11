39ee97fe8260645452ca3f720b12b9006ed46217
Dev Thakkar <90659457+DevT9@users.noreply.github.com>
2023-09-24 19:16:30 -0400

    Update NetScore.ts

--- src/controllers/NetScore.ts
+++ src/controllers/NetScore.ts
@@ -1,29 +1,23 @@
-import { correctness } from "../src/controllers/correctness";
-import { calculateBusFactor } from "../src/controllers/BusFactor";
-import { calculateRampUp } from "../src/controllers/RampUp";
-import { Responsiveness } from "../src/controllers/Responsiveness";
-import { License } from "../src/controllers/License";
+import { correctness } from "./correctness";
+import { calculateBusFactor } from "./BusFactor";
+import { calculateRampUp } from "./RampUp";
+import { Responsiveness } from "./Responsiveness";
+import { License } from "./License";
 
 export class NetScore {
     constructor(private owner: string, private repo: string) {
-    });
-    calculate(): number {
-        const correctnessobj = correctness(this.owner, this.repo);
+    };
+    async calculate(): Promise<{correctnessScore: number, busFactorScore: number, rampUpScore: number, responsivenessScore: number, licenseScore: number, netScore: number}> {
+        const correctnessobj = new correctness(this.owner, this.repo);
         const correctnessScore = await correctnessobj.check();
-        const busFactorScore = calculateBusFactor(this.owner, this.repo);
-        const rampUpScore = calculateRampUp(this.owner, this.repo);
-        
-        const responsivenessScore = Responsiveness.calculate(this.owner, this.repo);
-        const licenseScore = License.calculate();
-
+        const busFactorScore = await calculateBusFactor(this.owner, this.repo);
+        const rampUpScore = await calculateRampUp(this.owner, this.repo);
+        const responsiveness = new Responsiveness('someSharedProperty', this.owner, this.repo);
+        await responsiveness.fetchData();
+        const responsivenessScore = responsiveness.calculateMetric();
+        const license = new License('someSharedProperty', this.owner, this.repo);
+        const licenseScore = license.calculateMetric();
+        const netScore = (correctnessScore * 0.25 + busFactorScore * 0.15 + rampUpScore * 0.25 + responsivenessScore * 0.3 + licenseScore * 0.05);
         return {correctnessScore, busFactorScore, rampUpScore, responsivenessScore, licenseScore, netScore};
-    }
-}
-
-export class ScoreCalculator {
-    calculate(): number {
-        const netScore = new NetScore();
-        const score = netScore.calculate();
-        return score;
     }
 }
