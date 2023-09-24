64e8dd5261d707c7c11cc7e0dc62e6610de544ab
jonathandow <89482514+jonathandow@users.noreply.github.com>
2023-09-24 14:01:28 -0400

    Revert "Rampup merge"

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.ts'], // Adjust the path as needed
-    coverageReporters: ['lcov', 'text-summary'],
-  };
-  
-  
