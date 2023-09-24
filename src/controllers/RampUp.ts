c51dbc3ab2dcb18532bfc59a395abd053ccf5e54
jonathandow <89482514+jonathandow@users.noreply.github.com>
2023-09-24 15:20:54 -0400

    Removed unnecessary print statements

--- src/controllers/RampUp.ts
+++ src/controllers/RampUp.ts
@@ -11,8 +11,6 @@ export const calculateRampUp = async (
   next: NextFunction
 ) => {
   const { owner, repo } = req.query as { owner: string; repo: string };
-  console.log('owner: ', owner);
-  console.log('repo: ', repo);
   try {
     // Fetch data from GitHub API
     const contributors = await fetchRepositoryContributors(owner, repo);
@@ -31,9 +29,6 @@ export const calculateRampUp = async (
     const forksContribution = weights.Forks * forks.length;
 
     // Calculate the ramp-up score
-    console.log('Contributions: ', contributorsContribution);
-    console.log('Stars: ', starsContribution);
-    console.log('forks: ', forksContribution);
     let rampUpScore =
       (contributorsContribution + starsContribution + forksContribution) /
       (weights.Contributors + weights.Stars + weights.Forks);
