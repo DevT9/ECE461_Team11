99ae331eb819d648769d528f40cfa5a79858b40a
Rohan Sagar <rohansagar3202@gmail.com>
2023-10-23 21:25:10 -0400

    Update api.utils.ts

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-useless-catch */
 import axios from 'axios';
-// import { newToken } from '../index';
+import { newToken } from '../index';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getRequest = async (endpoint: string, params?: any) => {
   //console.log(newToken);
   const url = `https://api.github.com${endpoint}`;
-  const token = `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`;
+  const token = newToken;
   if (!token) {
     throw new Error('No bearer token found');
   }
