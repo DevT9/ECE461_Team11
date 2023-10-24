e6df7ad5c75d0ec54300c7cd5c3cdf682868e83f
Jonathan Merrill Dow <dow3@eceprog2.ecn.purdue.edu>
2023-10-23 22:12:06 -0400

    Fixed RampUp

--- __test__/RampUp.test.ts
+++ __test__/RampUp.test.ts
@@ -1,104 +1,39 @@
 import { calculateRampUp } from '../src/controllers/RampUp';
+import * as rampUpAPI from '../src/utils/RampUpAPI';
 
-jest.mock('../src/utils/RampUpAPI', () => ({
-  fetchRepositoryContributors: jest.fn(() => Promise.resolve([])),
-  fetchRepositoryStars: jest.fn(() => Promise.resolve([])),
-  fetchRepositoryForks: jest.fn(() => Promise.resolve([])),
-  fetchFirstCommitTime: jest.fn(() => Promise.resolve('2023-09-20T12:00:00Z')),
-}));
+// Mocking the behavior of the external calls to the API
+jest.mock('../src/utils/RampUpAPI');
+
+const mockedRampUpAPI = rampUpAPI as jest.Mocked<typeof rampUpAPI>;
 
 describe('calculateRampUp', () => {
-  it('calculates the ramp-up score correctly', async () => {
-    const mockResponse: any = {
-      json: jest.fn(),
-      status: jest.fn(() => mockResponse),
-      send: jest.fn(),
-    };
-
-    const {
-      fetchRepositoryContributors,
-      fetchRepositoryStars,
-      fetchRepositoryForks,
-      fetchFirstCommitTime,
-    } = require('../src/utils/RampUpAPI');
-
-    await calculateRampUp(
-      { query: { owner: 'github_owner', repo: 'repository_name' } } as any,
-      mockResponse as any
-    );
-
-    expect(fetchRepositoryContributors).toHaveBeenCalledWith('github_owner', 'repository_name');
-    expect(fetchRepositoryStars).toHaveBeenCalledWith('github_owner', 'repository_name');
-    expect(fetchRepositoryForks).toHaveBeenCalledWith('github_owner', 'repository_name');
-    expect(fetchFirstCommitTime).toHaveBeenCalledWith('github_owner', 'repository_name');
-    expect(mockResponse.json).toHaveBeenCalledWith({ rampUpScore: expect.any(Number) });
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  it('handles a case where fetchFirstCommitTime returns null', async () => {
-    // Mock fetchFirstCommitTime to return null
-    require('../src/utils/RampUpAPI').fetchFirstCommitTime = jest.fn(() => Promise.resolve(null));
+  it('should calculate the ramp-up score correctly', async () => {
+    mockedRampUpAPI.fetchRepositoryContributors.mockResolvedValue(new Array(5));
+    mockedRampUpAPI.fetchRepositoryStars.mockResolvedValue(new Array(10));
+    mockedRampUpAPI.fetchRepositoryForks.mockResolvedValue(new Array(3));
+    mockedRampUpAPI.fetchFirstCommitTime.mockResolvedValue(new Date(Date.now() - (180 * 24 * 60 * 60 * 1000)).toISOString());
 
-    const mockResponse: any = {
-      json: jest.fn(),
-      status: jest.fn(() => mockResponse),
-      send: jest.fn(),
-    };
+    const score = await calculateRampUp('sampleOwner', 'sampleRepo');
+    expect(score).toBeCloseTo(0.625);
 
-    await calculateRampUp(
-      { query: { owner: 'github_owner', repo: 'repository_name' } } as any,
-      mockResponse as any
-    );
-
-    // Expect that the code handles null firstCommitTime gracefully
-    expect(mockResponse.json).toHaveBeenCalledWith({ rampUpScore: expect.any(Number) });
+    expect(mockedRampUpAPI.fetchRepositoryContributors).toHaveBeenCalledWith('sampleOwner', 'sampleRepo');
+    expect(mockedRampUpAPI.fetchRepositoryStars).toHaveBeenCalledWith('sampleOwner', 'sampleRepo');
+    expect(mockedRampUpAPI.fetchRepositoryForks).toHaveBeenCalledWith('sampleOwner', 'sampleRepo');
+    expect(mockedRampUpAPI.fetchFirstCommitTime).toHaveBeenCalledWith('sampleOwner', 'sampleRepo');
   });
 
-  it('handles an error during API fetch', async () => {
-    // Mock one of the API functions to throw an error
-    require('../src/utils/RampUpAPI').fetchRepositoryContributors = jest.fn(() => {
-      throw new Error('API Error');
+  it('should handle errors and return 0', async () => {
+    mockedRampUpAPI.fetchRepositoryContributors.mockImplementationOnce(() => {
+      return Promise.reject(new Error('API Error'));
     });
 
-    const mockResponse: any = {
-      json: jest.fn(),
-      status: jest.fn(() => mockResponse),
-      send: jest.fn(),
-    };
-
-    await calculateRampUp(
-      { query: { owner: 'github_owner', repo: 'repository_name' } } as any,
-      mockResponse as any
-    );
-
-    // Expect that the code handles errors during API fetch
-    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    const score = await calculateRampUp('sampleOwner', 'sampleRepo');
+    expect(score).toEqual(0);
   });
 
-  it('handles a case with non-zero contributors, stars, forks, and first commit time', async () => {
-    // Mock the API functions to return non-empty results
-    require('../src/utils/RampUpAPI').fetchRepositoryContributors = jest.fn(() =>
-      Promise.resolve([{ login: 'user1' }, { login: 'user2' }])
-    );
-    require('../src/utils/RampUpAPI').fetchRepositoryStars = jest.fn(() =>
-      Promise.resolve([{ login: 'user3' }, { login: 'user4' }])
-    );
-    require('../src/utils/RampUpAPI').fetchRepositoryForks = jest.fn(() =>
-      Promise.resolve([{ login: 'user5' }, { login: 'user6' }])
-    );
-
-    const mockResponse: any = {
-      json: jest.fn(),
-      status: jest.fn(() => mockResponse),
-      send: jest.fn(),
-    };
-
-    await calculateRampUp(
-      { query: { owner: 'github_owner', repo: 'repository_name' } } as any,
-      mockResponse as any
-    );
-
-    // Expect that the code handles the case with non-zero contributors, stars, forks, and first commit time
-    expect(mockResponse.json).toHaveBeenCalledWith({ rampUpScore: expect.any(Number) });
-  });
+  // ... additional tests ...
 });
-
